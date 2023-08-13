1c61e74a675e6d839121f28d63b7770675613d8b
Utkarsh Dubey <utkarsh.dubey3jan@gmail.com>
2023-08-13 06:46:47 +0530

    Finished the login/signup screens

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,50 @@
+import { Metadata } from "next";
+import Link from "next/link";
+
+import { UserAuthForm } from "./user-auth-form";
+import ThemeToggle from "@/components/ThemeToggle";
+
+export const metadata: Metadata = {
+  title: "Authentication",
+  description: "Authentication forms built using the components.",
+};
+
+export default function AuthenticationPage() {
+  return (
+    <>
+      <div className="absolute right-5 top-5">
+        <ThemeToggle />
+      </div>
+      <div className="flex min-h-screen items-center justify-center">
+        <div className="lg:p-8 p-6">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">Login</h1>
+              <p className="text-sm text-muted-foreground">
+                Connect with your web3 wallet to get started
+              </p>
+            </div>
+            <UserAuthForm />
+            <p className="px-8 text-center text-sm text-muted-foreground">
+              By clicking continue, you agree to our{" "}
+              <Link
+                href="/terms"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Terms of Service
+              </Link>{" "}
+              and{" "}
+              <Link
+                href="/privacy"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Privacy Policy
+              </Link>
+              .
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
